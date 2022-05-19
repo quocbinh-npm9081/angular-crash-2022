e36596fa798508e050b0a9e1515f3772a6d84584
quocbinh-npm9081 <quocbinhgithub@gmail.com>
2022-05-19 19:05:33 +0700

    adding product item render data into table

---
+++ src/app/Components/ShoppingCart/ShoppingCart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ShoppingCart',
+  templateUrl: './ShoppingCart.component.html',
+  styleUrls: ['./ShoppingCart.component.scss']
+})
+export class ShoppingCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HeaderComponent } from './Components/Header/Header.component';
 import { CardComponent } from './Components/Card/Card.component';
 import { CounterComponent } from './Components/Counter/Counter.component';
 import { WishMessageComponent } from './Components/WishMessage/WishMessage.component';
+import { ProductItemComponent } from './Components/ProductItem/ProductItem.component';
+import { ShoppingCartComponent } from './Components/ShoppingCart/ShoppingCart.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CardComponent,
     CounterComponent,
-    WishMessageComponent
+    WishMessageComponent,
+    ProductItemComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
