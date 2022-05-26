7377bedbb73a6497a1ff82daa63b1d3df6ac12f2
quocbinh-npm9081 <quocbinhgithub@gmail.com>
2022-05-26 19:30:49 +0700

    adding Handling request errors

--- src/app/Components/user/UserList/UserList.component.ts
+++ src/app/Components/user/UserList/UserList.component.ts
@@ -10,16 +10,24 @@ import { UserSevService } from 'src/app/Sevices/userSev.service';
 export class UserListComponent implements OnInit {
 
   public users: IUser[] = [] as IUser[];
-
+  public errorMsgs: string | undefined = undefined;
   constructor(private userSev: UserSevService) {
     //this.user = this.userSev.getUser();  // ko nen khai bao trong ham tao
 
   }
 
   ngOnInit() {
+    // this.userSev.getUser().subscribe((data: IUser[]) => // <--- life circle 
+    //   this.users = data
+    // );
+  }
+
+  public onHandleGetUser(): any {
+
     this.userSev.getUser().subscribe((data: IUser[]) => // <--- life circle 
-      this.users = data
+    { this.users = data }, (error) => this.errorMsgs = error
     );
+
   }
 
 }
--- src/app/Sevices/userSev.service.ts
+++ src/app/Sevices/userSev.service.ts
@@ -1,20 +1,36 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IUser, IGeo, IAddress, ICompany } from '../Models/User';
+import { IUser } from '../Models/User';
+import { throwError, catchError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserSevService {
 
-  constructor(private httpClient: HttpClient) {
 
-
-  }
+  constructor(private http: HttpClient) { }
 
   public getUser(): Observable<IUser[]> {
-    const URLdata = 'https://jsonplaceholder.typicode.com/users';
-    return this.httpClient.get<IUser[]>(URLdata)
+    const URLdata = 'https://jsonplaceholder.typicode.com/userss';
+    return this.http.get<IUser[]>(URLdata).pipe(catchError(this.handleError));
+  };
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      //  máy khác hoặc mạng bị lỗi
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      //backend trả dữ liệu không thành công
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
+
 
 }
